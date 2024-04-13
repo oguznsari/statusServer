88c5ef7abd5586b7a3a3b307910b88659bf80fa6
Oğuzhan SARI <oguzn.sari@gmail.com>
2024-04-13 19:31:01 +0300

    api to find active selcuksports url

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,58 @@
+"use client";
+import axios from "axios";
+import React, { useState } from "react";
+
 import { Button } from "@/components/ui/button";
 import UserAvatar from "@/components/user-avatar";
+import Link from "next/link";
+
+async function searchForActiveLink(setLink: any) {
+  try {
+    const response = await axios.get("/api/search");
+    if (response.status === 200) {
+      setLink(response.data.url);
+    }
+    console.log(response.data);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [link, setLink] = useState(false);
+
+  const handleButtonClick = async () => {
+    setIsLoading(true);
+    setLink(false);
+    await searchForActiveLink(setLink);
+    setIsLoading(false);
+  };
+
   return (
     <div className="flex flex-col justify-center items-center my-10">
       <h1>Selçuk spor link finder</h1>
       <UserAvatar />
-      <Button variant={"outline"} size={"lg"}>
-        Search the active link
+      <Button
+        variant={"outline"}
+        size={"lg"}
+        onClick={handleButtonClick}
+        disabled={isLoading}
+      >
+        {isLoading ? "Searching..." : "Search the active link"}
       </Button>
+      {link && (
+        <div className="my-10 flex justify-center">
+          <div className="flex flex-col items-center">
+            <span className="text-2xl mb-2">🥳</span>
+            <Button variant={"destructive"}>
+              <Link href={link} passHref target="_blank">
+                {link}
+              </Link>
+            </Button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
---
+++ pages/api/search.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const startIndex = 1187; // latest active index
+const loopLimit = 50;
+const requestTimeout = 4000; // 4 seconds in milliseconds
+
+export default async function search(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  for (let i = 0; i <= loopLimit; i++) {
+    let currentIndex = startIndex + i;
+    let url = `https://www.selcuksportshd${currentIndex}.xyz`;
+
+    try {
+      const response = await axios.get(url, { timeout: requestTimeout });
+      if (response.status === 200) {
+        res.status(200).json({ success: true, url });
+        return; // Exit the loop if successful response is received
+      } else {
+        console.log(`URL ${url} returned status ${response.status}`);
+      }
+    } catch (error) {
+      console.log(`catch: URL ${url} failed to fetch:`, error.message);
+    }
+  }
+
+  console.log("No active link found");
+  res.status(404).json({ success: false, message: "No active link found" });
+}
